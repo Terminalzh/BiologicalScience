f34a7290c8123e44a6f4e1c589fda63e85e0aaa5
“Terminal” <2715654547@qq.com>
2023-06-04 17:19:11 +0800

    完成轮播图管理

---
+++ backend/src/main/java/com/neutech/mammalia/controller/BannerController.java
@@ -0,0 +1,89 @@
+package com.neutech.mammalia.controller;
+
+import com.neutech.mammalia.bean.Banner;
+import com.neutech.mammalia.service.BannerService;
+import jakarta.annotation.Resource;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping(value = "/banner")
+public class BannerController {
+    @Resource
+    private BannerService bannerService;
+
+    @PostMapping
+    public Map<String, Object> addBanner(@RequestBody Banner banner) {
+        Map<String, Object> map = new HashMap<>();
+        if (bannerService.addBanner(banner) == 1) {
+            map.put("code", HttpStatus.CREATED.value());
+            map.put("message", HttpStatus.CREATED.getReasonPhrase());
+        } else {
+            map.put("code", HttpStatus.BAD_REQUEST.value());
+            map.put("message", HttpStatus.BAD_REQUEST.getReasonPhrase());
+        }
+        return map;
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public Map<String, Object> deleteBannerById(@PathVariable Integer id) {
+        Map<String, Object> map = new HashMap<>();
+        if (bannerService.deleteBannerById(id) == 1) {
+            map.put("code", HttpStatus.NO_CONTENT.value());
+            map.put("message", HttpStatus.NO_CONTENT.value());
+        } else {
+            map.put("code", HttpStatus.NOT_FOUND.value());
+            map.put("message", HttpStatus.NOT_FOUND.getReasonPhrase());
+        }
+        return map;
+    }
+
+    @PutMapping(value = "/{id}")
+    public Map<String, Object> updateBannerById(@PathVariable Integer id, @RequestBody Banner banner) {
+        Map<String, Object> map = new HashMap<>();
+        banner.setId(id);
+        if (bannerService.updateBannerById(banner) == 1) {
+            map.put("code", HttpStatus.OK.value());
+            map.put("message", HttpStatus.OK.value());
+        } else {
+            map.put("code", HttpStatus.BAD_REQUEST.value());
+            map.put("message", HttpStatus.BAD_REQUEST.getReasonPhrase());
+        }
+        return map;
+    }
+
+    @GetMapping("/{id}")
+    public Map<String, Object> inquireBannerById(@PathVariable Integer id) {
+        Map<String, Object> map = new HashMap<>();
+        Banner banner = bannerService.inquireBannerById(id);
+        if (banner != null) {
+            map.put("code", HttpStatus.OK.value());
+            map.put("message", HttpStatus.OK.value());
+            map.put("data", banner);
+        } else {
+            map.put("code", HttpStatus.NOT_FOUND.value());
+            map.put("message", HttpStatus.NOT_FOUND.getReasonPhrase());
+        }
+        return map;
+    }
+
+    @GetMapping
+    public Map<String, Object> inquireAllBanner() {
+        Map<String, Object> map = new HashMap<>();
+        List<Banner> banners = bannerService.inquireAllBanner();
+        if (banners.size() > 0) {
+            map.put("code", HttpStatus.OK.value());
+            map.put("message", HttpStatus.OK.value());
+            map.put("data", banners);
+        } else {
+            map.put("code", HttpStatus.NOT_FOUND.value());
+            map.put("message", HttpStatus.NOT_FOUND.getReasonPhrase());
+        }
+        return map;
+    }
+
+}
